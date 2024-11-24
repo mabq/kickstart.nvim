296452d7c90d7bd2b3c0df7ceaea7383ecf9b886
mabq <alejandro.banderas@me.com>
2024-11-24 00:35:05 -0500

    all

---
+++ lua/mabq/lazy/treesitter.lua
@@ -0,0 +1,58 @@
+-- `:help lsp-vs-treesitter`
+-- `:help nvim-treesitter`
+
+-- If you get an error, try syncing all plugins with Lazy.
+
+return {
+  'nvim-treesitter/nvim-treesitter',
+  build = ':TSUpdate',
+  config = function()
+    require('nvim-treesitter.configs').setup {
+      -- A list of parser names, or "all"
+      ensure_installed = {
+        'bash',
+        'css',
+        'diff',
+        'go',
+        'html',
+        'javascript',
+        'jsdoc',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'query',
+        'rust',
+        'typescript',
+        'vim',
+        'vimdoc',
+      },
+
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
+
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don"t have `tree-sitter` CLI installed locally
+      auto_install = true,
+
+      indent = {
+        enable = true,
+      },
+
+      highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
+
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on "syntax" being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = { 'markdown' },
+      },
+    }
+  end,
+  -- Additional modules that might interest you:
+  --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+  --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+  --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+}
